1c778c67a645d8e823bf843d49c6adeccb9d204e
hanseroland <rolandntougou@gmail.com>
2026-03-14 23:59:08 +0000

    feat(auth): plug validation middleware on register and login routes

--- semaine-2/src/interfaces/routes/authRoutes.js
+++ semaine-2/src/interfaces/routes/authRoutes.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const validateMiddleware = require('../middlewares/validateMiddleware');
+const { registerSchema, loginSchema } = require('../validators/authValidator');
+
 
 const createAuthRouter = (authController) => {
     const router = express.Router();
 
-    router.post('/register', (req, res) => authController.register(req, res));
-    router.post('/login', (req, res) => authController.login(req, res));
+    router.post('/register',
+        validateMiddleware(registerSchema),
+        (req, res) => authController.register(req, res));
+
+    router.post('/login',
+        validateMiddleware(loginSchema),
+        (req, res) => authController.login(req, res));
 
     return router;
 };
