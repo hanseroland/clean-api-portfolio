29a4067d3ba80918c1611b927b2ea6bb0904a4cf
hanseroland <rolandntougou@gmail.com>
2026-03-14 23:58:57 +0000

    feat(validation): add validateMiddleware for request validation

---
+++ semaine-2/src/interfaces/middlewares/validateMiddleware.js
@@ -0,0 +1,17 @@
+const validateMiddleware = (schema) => {
+
+    return (req, res, next) => {
+
+        const { error } = schema.validate(req.body);
+
+        if (error) {
+            return res.status(400).json({ success: false, message: error.details[0].message });
+        }
+
+        next();
+
+    }
+
+}
+
+module.exports = validateMiddleware;
