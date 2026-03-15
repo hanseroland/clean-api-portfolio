d862bc63c52ca486280a691ec07445991347538d
hanseroland <rolandntougou@gmail.com>
2026-03-15 00:21:29 +0000

    feat(app): plug error middleware after all routes

--- semaine-2/src/app.js
+++ semaine-2/src/app.js
@@ -20,6 +20,7 @@ const AuthController = require('./interfaces/controllers/AuthController');
 const UserController = require('./interfaces/controllers/UserController')
 const createAuthRouter = require('./interfaces/routes/authRoutes');
 const createUserRouter = require('./interfaces/routes/userRoutes')
+const errorMiddleware = require('./interfaces/middlewares/errorMiddleware');
 
 
 app.use(express.json());
@@ -41,10 +42,11 @@ const api = process.env.API_URL || '/api';
 app.use(`${api}/auth`, createAuthRouter(authController));
 app.use(`${api}/users`, createUserRouter(userController, authmiddleWare));
 
+// Middleware global de gestion des erreurs
+app.use(errorMiddleware);
 
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
